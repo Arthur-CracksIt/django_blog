209823ef6847ae9ebc3e8138fbf341555ec24027
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-10-21 14:21:08 +0000

    added the custom admin view

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'slug', 'author', 'published_date', 'status']
+    list_filter = ['author', 'published_date', 'created_at', 'status']
+    search_fields = ['title', 'body']
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ['author']
+    date_hierarchy = 'published_date'
+    ordering = ['status', 'published_date']
+    show_facets = admin.ShowFacets.ALWAYS
