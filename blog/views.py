1c1306fcae907025f8fa0c8013cd4c6f82736f15
Arthur-CracksIt <davidkwadwoarthur.tech@gmail.com>
2025-10-22 15:01:17 +0000

    create the urls for the blog application and added it to the main urls file

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 # Create your views here.
 
@@ -12,10 +12,15 @@ def post_list(request):
 
 from django.http import Http404
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found")
+    # try:
+    #     post = Post.published.get(id=id)
+    # except Post.DoesNotExist:
+    #     raise Http404("No Post found")
+    post = get_object_or_404(
+        Post,
+        id = id,
+        status = Post.Status.PUBLISHED
+    )
     return render(
         request,
         'blog/post/detail.html',
